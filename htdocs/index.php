ec7e4cd889f98efd7fac41a7b47626d0f9878d89
longmeaghan <long.meaghan.e@gmail.com>
2018-07-21 04:34:48 -0700

    Added header.php
    + Created header.php
    + Applied php line to all other php pages
    + Works correctly on MAMP

---
+++ htdocs/index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="index.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <!-- Font - IBM Plex Mono -->
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono" rel="stylesheet">
+    <title>Meaghan Long</title>
+  </head>
+  <body>
+
+    <?php include 'header.php';?>
+
+      <div class="row call-to-action">
+        <div class="col">
+          <h1>solving ux/ui enterprise solutions for over 1 million users</h1>
+          <span class="subtitle">Component patterns, responsive design, copywriting, competitor research, and acessibility</span>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4">
+          <span class="section-title">ArcGIS Online</span>
+          <p>Lead UX/UI Designer • web app<br>2017 - present</p>
+          <p>Reimagining experiences with GIS software through design, patterns, copy, and mobility</p>
+        </div>
+        <div class="col-lg-4">
+          <span class="section-title">SIGGRAPH Portal</span>
+          <p>Lead UX/UI Designer • web app<br>2016 - present</p>
+          <p>Passion volunteer project that’s increased application submissions by 18%</p>
+        </div>
+        <div class="col-lg-4">
+          <span class="section-title">Calcite</span>
+          <p>Project manager + UI Designer • design system<br>2016 - present</p>
+          <p>Unifying Esri products and giving empowerment by providing easy access</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4">
+          <span class="section-title">Insights</span>
+          <p>UI Designer • web app<br>2016 - 2017</p>
+          <p>Bringing data and GIS to life with data visualization</p>
+        </div>
+        <div class="col-lg-4">
+          <span class="section-title">ArcGIS Maps for Office</span>
+          <p>Product designer • Microsoft Office plugin<br>2016 - 2017</p>
+          <p>Eloving GIS without leaving Excel</p>
+        </div>
+        <div class="col-lg-4">
+          <span class="section-title">SHOCKOE</span>
+          <p>UX/UI Designer • website<br>2016</p>
+          <p>Highlighting Shockoe's mobile application solutions</p>
+        </div>
+      </div>
+
+    </div>
+
+  </body>
+</html>
---
+++ htdocs/siggraph.php
@@ -0,0 +1,206 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="stylesheet" href="index.css">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+  <!-- Font - IBM Plex Mono -->
+  <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono" rel="stylesheet">
+  <title>Meaghan Long</title>
+</head>
+
+<body>
+  <div class="container-fluid">
+
+    <?php include 'header.php';?>
+
+    <div class="row shockoe-section">
+      <div class="col-md-3">
+
+        <div class="cs-title">
+          <h1>SIGGRAPH</h1>
+          <ul>
+            <li>UX/UI Designer + Frontend Developer
+            </li>
+            <li>Website applications
+            </li>
+            <li>2016 - present</li>
+          </ul>
+        </div>
+
+        <div class="team">
+          <h6>Team</h6>
+          <p>
+            <a href="#">Samantha Carbonell</a>. <a href="#">Nikki Ziegler</a>. Meaghan Long.
+          </p>
+        </div>
+
+        <div class="anchor-links">
+          <h6>Topics</h6>
+          <ul>
+            <li><a href="#users">Users</a></li>
+            <li><a href="#application">Application</a></li>
+            <li><a href="#student-profiles">Student profiles</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md-9">
+        <div class="text-center">
+          <img src="../imgs/info-img.png" class="img-fluid" alt="Responsive image">
+        </div>
+
+        <div class="row highlights">
+
+          <div class="col-md-12">
+            <h2 class="header2">Highlights</h2>
+          </div>
+
+          <div class="col-md-4">
+            <p>
+              <strong>Challenge</strong>
+              <br> Create an intuitive system for international students to apply to
+            </p>
+          </div>
+
+          <div class="col-md-4">
+            <p class="highlight-border">
+              <strong>Opportunity</strong>
+              <br> Address user pain points and provide easier workflows to complete applications
+            </p>
+          </div>
+
+          <div class="col-md-4">
+            <p class="highlight-border">
+              <strong>Outcome</strong>
+              <br> Increased student applications by 18%. Next challenge is reducing support contact.
+            </p>
+          </div>
+
+        </div>
+
+        <div class="cs-paragraphs">
+          <h2 class="header2">Summary</h2>
+          <p>The Student Volunteer Program wanted a more streamlined stronger experience as early on as the application process, and that was going to be accomplished by replacing a system that hadn’t been updated since being built in the early 2000s. Most
+            of my focus involved <a href="#">Test</a> me leading workflows and designs to make the student application easier to understand and complete. The team was able to accomplish this by student applications rising by 18%.</p>
+          <p>We have been successfully growing the system by having a retrospective after each milestone of the student application process, based off of the support emails we received from students. This allowed us to document and plan for designs and developments
+            the following year.</p>
+          <p>My time on this system will end in August 2019, but this has been an important project in my growth. It has built upon my knowledge on system designs, and taught me how consistent UX/UI expedites a user’s workflow. Finally, this has helped my
+            remote communication and etiquette.</p>
+          <p>My new goal for this system during my time left is take my experience as Communcation Lead for the Student Volunteer Sub Committee, and make additions to the software to receduce the amount of support email we receive.</p>
+
+          <div id="users">
+            <h2 class="header2">Users</h2>
+            <p>When beginning this project, we identitied our three types of users that would have access to this system, along with their goals and pain points. This allowed us to make stronger decisions on the experience by using this a reference. </p>
+
+            <div class="row">
+              <div class="col-md-2 text-center">
+                <img src="../imgs/s1.png" class="img-fluid" alt="Responsive image">
+              </div>
+
+              <div class="col-md-4">
+                <strong>Admin</strong> <br>
+                <p>Subcommitte members need to have all data available of every student who has signed up within the Student Volunteer program.</p>
+
+              </div>
+              <div class="col-md-6">
+
+                <ul class="goals-and-points">
+                  <li>Have students complete application</li>
+                  <li>Review for financial assistance candidates</li>
+                  <li>Know all student's schedules</li>
+                </ul>
+
+                <ul class="goals-and-points">
+                  <li>Students contacting support about application</li>
+                  <li>Choosing financial assistance candidates</li>
+                  <li>Information of all students during the conference</li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="row yellow-border-top">
+              <div class="col-md-2 text-center">
+                <img src="../imgs/s2.png" class="img-fluid" alt="Responsive image">
+              </div>
+
+              <div class="col-md-4">
+                <strong>Viewer with permissions</strong> <br>
+                <p>Team leaders are top performing student volunteers who are given limited access of all student volunteers.</p>
+
+              </div>
+              <div class="col-md-6">
+                <ul class="goals-and-points">
+                  <li>Complete Student Volunteer application</li>
+                  <li>Scoring quickly student volunteer essays</li>
+                  <li>Digesting shift information while on the run</li>
+                </ul>
+
+                <ul class="goals-and-points">
+                  <li>Understanding the application</li>
+                  <li>Unbiasly socre student essays</li>
+                  <li>Processing complex shifts</li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="row yellow-border-top">
+              <div class="col-md-2 text-center">
+                <img src="../imgs/s3.png" class="img-fluid" alt="Responsive image">
+              </div>
+
+              <div class="col-md-4">
+                <strong>Viewer</strong> <br>
+                <p>Student volunteers are only able to access their own information</p>
+
+              </div>
+              <div class="col-md-6">
+
+                <ul class="goals-and-points">
+                  <li>Completing Student Volunteer application</li>
+                  <li>Accepting or declining position if offered</li>
+                  <li>Understanding their schedule</li>
+                </ul>
+
+                <ul class="goals-and-points">
+                  <li>Understanding the application</li>
+                  <li>Accepting or declining position</li>
+                  <li>Understanding shift schedule</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div id="application">
+            <h2 class="header2">Application</h2>
+            <p>The first tasked we had for the system was having the application available that would be understood both by Student volunteers and Team leaders. In the pass, the form was not only difficult to understand, was not clear what was innacruately
+              filled out, and how many characters a student typed into a form, or even what section of the form a student was in, or how much of the form they had left. But within one month, paint points of the form were reviewed, addressing it by having
+              stronger error states, character counts at the bottom of the form, and having a page indicator at the top of the page, highlighting the current page a student was on.</p>
+          </div>
+
+          <div id="student-profiles">
+            <h2 class="header2">Profile</h2>
+            <p>The largest struggle during the week at the conference is the schedule of shifts. Bwetween students not fully understanding specific information, team leaders working around many shifts, to the subcomittee getting up to date information on
+              students, it can be a lot of confusion and spending precious time re-explaining information to many people.</p>
+            <p>To fix this, we have an student volutneer and team leader profile that gives this information clearly. Breaking it down, a subcomittee can view all information of a student volunteer, including quickly seeing which programs and team leaders
+              they work with and their schedule. Information can be drilled further by click on a time and a modal appearing with an overview of their performance, including whether they showed up for a shift or not.</p>
+            <p>For a team leader, being able to quickly scan a list of student volunteers along with checking them in and out is important in order to streamline setup time and switching students in a program. Appearing on their modal is a table with a list
+              of actions per student, including an option to write a review.</p>
+            <p>Lastly, we have student volunteers who have two main points. Not clearing knowing what tiem their shift is, taking a while to grasp that they need to be at the office 30 minutes before their shift for setup and training, and knowing who their
+              team leader will be for that shift. For this we add all of this information to their modal, including having a picture of their TL that will make it easier for the student to quickly identity their lead.</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+</body>
+
+</html>

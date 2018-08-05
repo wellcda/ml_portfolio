3fcf8daf834e6bdf603074a15411f301749e6187
longmeaghan <long.meaghan.e@gmail.com>
2018-08-05 12:33:17 -0700

    Added copy

--- htdocs/insights.php
+++ htdocs/insights.php
@@ -6,6 +6,77 @@
   </head>
   <body>
     <?php include 'header.php';?>
+    <div class="container-fluid">
+
+      <div class="row shockoe-section">
+        <div class="col-md-3">
+          <div class="fixed-left-column">
+            <div class="shockoe-header">
+              <h1>Insights for ArcGIS</h1>
+              <ul class="description-list">
+                <li>Product designer </li>
+                <li>Web application </li>
+                <li>2016-2017</li>
+              </ul>
+            </div>
+
+            <div class="team">
+              <p>
+                <strong>Team</strong>
+                <br>
+                Meaghan Long.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-9">
+          <div class="text-center">
+            <img src="../img/info-img.png" class="img-fluid" alt="Responsive image">
+          </div>
+
+          <div class="row highlights">
+
+            <div class="col-md-12">
+              <h2 class="header2">Highlights</h2>
+            </div>
+
+            <div class="col-md-4">
+              <p>
+                <strong>Challenge</strong>
+                <br>
+                Allow batch editing across cards.
+              </p>
+            </div>
+
+            <div class="col-md-4">
+              <p class="highlight-border">
+                <strong>Opportunity</strong>
+                <br>
+                Focuse more on UX as UI decisions were limited.
+              </p>
+            </div>
+
+            <div class="col-md-4">
+               <p class="highlight-border">
+                <strong>Outcome</strong>
+                <br>
+                A solution that expedited users time and effort.
+              </p>
+            </div>
+
+          </div>
+
+          <div class="row text-description">
+            <h2 class="header2">Summary</h2>
+            <p>Within one sprint, I worked on extending utility to tools found within cards by enabling batch-editing with the same tools across multiple cards. This was more focused on the user experience side than user interface as the goal was to validate each tool to determine whether it made sense to allow users to perform batch work on that particular tool. While for some tools it made sense to create a batch version to expedite the users' workflow, other tools would have required too many extra steps to configure, hence reducing the overall return of having batch capabilities. The most important criteria was that the tool could be applied to several types of data cards, and also supported changes to data in real time if a map’s extent on the card was adjusted.
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
   </body>
   <footer>
     <p>Meaghan Long, 2018. <a href="https://github.com/longmeaghan/ml_portfolio">Github repository</a></p>

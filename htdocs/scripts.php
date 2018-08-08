e0accc06f38d190817131ef9dd9eb569f7693fb2
Wellington Aguiar <wellcda@gmail.com>
2018-08-07 23:27:30 -0300

    Adding CDNs

--- htdocs/scripts.php
+++ htdocs/scripts.php
@@ -1,2 +1,2 @@
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="js/lightgallery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.6.11/js/lightgallery-all.js" integrity="sha256-gXeMdTGcUOelrz6voAnccIKd5yNP44W3t9hKtztLv6U=" crossorigin="anonymous"></script>
